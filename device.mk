fb7f49b1529ffc0912cd4a0eede5f912dcae6e24
maxwen <max.weninger@gmail.com>
2021-10-26 00:07:24 +0200

    rpi4: and we have liftoff on 12
    fix building libinit
    fix ueventd.rc location and naming
    sync device.mk and manifest files with yukawa
    all tmp changes to make it build and boot
    
    Change-Id: Ia1a2d65f0823df185c0797b29a587a8a44c4d15b

--- device.mk
+++ device.mk
@@ -16,18 +16,27 @@
 
 DEVICE_PATH := device/brcm/rpi4
 
-include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
+# Enable Scoped Storage related
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := mdpi
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Audio
+# Build default bluetooth a2dp and usb audio HALs
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.usb.default \
+    audio.primary.yukawa \
     audio.r_submix.default \
-    audio.usb.default
+    audio.bluetooth.default \
+    audio.hearing_aid.default \
+    audio.a2dp.default
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 #PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service.rpi4 \
@@ -36,17 +45,20 @@ PRODUCT_PACKAGES += \
     gralloc.rpi4
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.2-impl \
+    android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     hwcomposer.drm \
     gralloc.gbm
 
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
+
+#PRODUCT_PACKAGES += \
+    memtrack.rpi4
 
 # egl
 PRODUCT_PACKAGES += \
@@ -57,14 +69,13 @@ PRODUCT_PACKAGES += \
     libGLESv1_CM_swiftshader \
     libGLESv2_swiftshader
 
-# Keymaster HAL
+# Software Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service.software
+
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
-
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service.software
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -72,9 +83,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey
 
-# camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-external-service
+# Enable USB Camera
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service
 
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service \
@@ -127,7 +138,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(DEVICE_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
      $(DEVICE_PATH)/hostapd/hostapd_default.conf:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd_default.conf \
      $(DEVICE_PATH)/hostapd/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
      $(DEVICE_PATH)/hostapd/hostapd.deny:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.deny
@@ -137,9 +148,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service.rpi4 \
     btuart
 
-# USB HAL
+# USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.1-service
 
 # media configurations
 PRODUCT_COPY_FILES += \
@@ -156,15 +167,14 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_tv.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_tv.xml \
-    $(DEVICE_PATH)/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_tv.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_tv.xml
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/init.vendor.rpi4.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rpi4.rc \
     $(DEVICE_PATH)/init.rpi4.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rpi4.usb.rc \
-    $(DEVICE_PATH)/ueventd.rpi4.rc:root/ueventd.rpi4.rc \
-    $(DEVICE_PATH)/init.system.rpi4.rc:root/init.rpi4.rc \
     $(DEVICE_PATH)/fstab.rpi4:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.rpi4 \
+    $(DEVICE_PATH)/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
+    $(DEVICE_PATH)/init.system.rpi4.rc:root/init.rpi4.rc \
     $(DEVICE_PATH)/fstab.rpi4:$(TARGET_COPY_OUT_RAMDISK)/fstab.rpi4
 
 PRODUCT_COPY_FILES += \
@@ -172,7 +182,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     audio.primary.rpi4 \
-    memtrack.rpi4 \
     gps.rpi4 \
     power.rpi4
 
@@ -182,7 +191,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libinit_rpi4
 
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
