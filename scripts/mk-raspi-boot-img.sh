1ae20a0c40ac38f0a6384c7dcea21044fb44824c
maxwen <max.weninger@gmail.com>
2022-04-15 16:30:04 +0200

    rpi4: update helper scripts
    Change-Id: Id54c82fa34914a6d7483b5b5986c91c95899b98f

--- scripts/mk-raspi-boot-img.sh
+++ scripts/mk-raspi-boot-img.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-#IN_IMAGE_DIR=$OUT_DIR/target/product/rpi4/
-#IN_BOOT_FILES=$ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/
-#OUT_IMAGE_FILE=$HOME/raspberrypi/omni-$ROM_BUILDTYPE.img
-
-if [ -z $ROM_BUILDTYPE ]; then
-    echo "missing ROM_BUILDTYPE"
-    exit 0
+if [ -z $OUT_DIR ]; then
+    OUT_DIR=`pwd`/out
 fi
+
+IN_IMAGE_DIR=$OUT_DIR/target/product/rpi4/
+IN_BOOT_FILES=$ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/
+OUT_IMAGE_FILE=$HOME/raspberrypi/boot.img
 
 options=$(getopt -o ho:i:b: -- "$@")
 [ $? -eq 0 ] || { 
@@ -22,19 +21,14 @@ while true; do
         OUT_IMAGE_FILE=$1
         shift
         ;;
-    -i)
-        shift
-        IN_IMAGE_DIR=$1
-        shift
-        ;;
     -b)
         shift
         IN_BOOT_FILES=$1
         shift
         ;;
     -h)
-        echo "-i <image folder> -b <boot file dir> -o <image file>"
-        echo "e.g. -i $OUT_DIR/target/product/rpi4/ -b $ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/ -o /tmp/omni.img"
+        echo "-b <boot file dir> -o <image file>"
+        echo "e.g. -b $ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/ -o /tmp/omni.img"
         exit 0
         ;;
     --)
@@ -44,11 +38,6 @@ while true; do
     esac
 done
 
-if [ -z $IN_IMAGE_DIR ]; then
-    echo "missing -i <image folder>"
-    exit 0
-fi
-
 if [ -z $OUT_IMAGE_FILE ]; then
     echo "missing -o <image file>"
     exit 0
@@ -56,16 +45,6 @@ fi
 
 if [ -z $IN_BOOT_FILES ]; then
     echo "missing -b <boot file dir>"
-    exit 0
-fi
-
-if  [ ! -f "$IN_IMAGE_DIR/system.img" ]; then
-    echo "no <input folder>/system.img"
-    exit 0
-fi
-
-if  [ ! -f "$IN_IMAGE_DIR/vendor.img" ]; then
-    echo "no <input folder>/vendor.img"
     exit 0
 fi
 
@@ -84,42 +63,26 @@ if  [ ! -f "$IN_BOOT_FILES/config.txt" ]; then
     exit 0
 fi
 
-echo "create: images $IN_IMAGE_DIR + boot files $IN_BOOT_FILES -> $OUT_IMAGE_FILE"
+echo "create: boot files $IN_BOOT_FILES -> $OUT_IMAGE_FILE"
 
 if [ -f $OUT_IMAGE_FILE ]; then
     rm $OUT_IMAGE_FILE
 fi
 
 echo "create empty image"
-dd if=/dev/zero of="$OUT_IMAGE_FILE" bs=1M count=8192
+dd if=/dev/zero of="$OUT_IMAGE_FILE" bs=1M count=128
 
 echo "create partitions"
 sudo sfdisk "$OUT_IMAGE_FILE"  << EOF
-,+128M,0xC,*
-,+2G,0x83,-
-,+256M,0x83,-
-,,0x83,-
+,,0xC,*
 EOF
 
-echo "mount partitions"
-sudo kpartx -av "$OUT_IMAGE_FILE"
-
 echo "create file systems"
-sudo mkfs.vfat /dev/mapper/loop0p1 -n boot
-sudo mkfs.ext4 /dev/mapper/loop0p2 -L system
-sudo mkfs.ext4 /dev/mapper/loop0p3 -L vendor
-sudo mkfs.ext4 /dev/mapper/loop0p4 -L userdata
-echo "enable project quota"
-sudo tune2fs -O project,quota /dev/mapper/loop0p4
-
-echo "write system.img"
-sudo dd if="$IN_IMAGE_DIR/system.img" of=/dev/mapper/loop0p2 bs=1M
-echo "write vendor.img"
-sudo dd if="$IN_IMAGE_DIR/vendor.img" of=/dev/mapper/loop0p3 bs=1M
+sudo mkfs.vfat $OUT_IMAGE_FILE -n boot
 
 echo "write boot patition"
 sudo mkdir /mnt/tmp
-sudo mount /dev/mapper/loop0p1 /mnt/tmp
+sudo mount $OUT_IMAGE_FILE /mnt/tmp
 sudo cp "$IN_IMAGE_DIR/ramdisk.img" /mnt/tmp/
 sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
 sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp/
@@ -134,9 +97,5 @@ sync
 echo "unmounting"
 sudo umount /mnt/tmp
 sudo rm -fr /mnt/tmp
-
-sudo kpartx -dv "$OUT_IMAGE_FILE"
-
-echo "now write $OUT_IMAGE_FILE to a sdcard"
 
 exit 1
