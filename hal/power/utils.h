7e8c1e5185feaaaddf29e8d67f4f8f7adfaafd3b
maxwen <max.weninger@gmail.com>
2022-04-25 21:35:04 +0200

    power: implement setInteractive API
    limit scaling max to min freq on screen off
    
    Change-Id: Ia86642c163e0984a5e3791eba63d2a65b62bbe53

--- hal/power/utils.h
+++ hal/power/utils.h
@@ -19,11 +19,15 @@
 #define SCALING_GOVERNOR_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 #define SCALING_MAX_FREQ "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
 #define SCALING_MIN_FREQ "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+#define CPUINFO_MIN_FREQ "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
+#define CPUINFO_MAX_FREQ "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
 
 int sysfs_read(char *path, char *s, int num_bytes);
 int sysfs_write(char *path, char *s);
 int get_scaling_governor(char governor[], int size);
 int get_scaling_max_freq(char max_freq[], int size);
 int get_scaling_min_freq(char min_freq[], int size);
+int get_cpuinfo_min_freq(char min_freq[], int size);
+int get_cpuinfo_max_freq(char max_freq[], int size);
 
 #endif //__UTILS_H__
