2ff7a32e065627e9b3a4186445d76137013a9a61
maxwen <max.weninger@gmail.com>
2022-04-29 11:52:01 +0200

    rpi4: there is no hdmi audio at the moment
    so fix everything to headphone jack - card 0
    
    Change-Id: I301ab9aeaf3ea6e1014e5911ff42fe9f1b837140

--- hal/audio/audio_hw.c
+++ hal/audio/audio_hw.c
@@ -89,11 +89,14 @@ struct stub_stream_in {
 };
 
 static int get_pcm_card() {
-    return property_get_int32("audio.pcm.card", PCM_CARD);
+    // TODO
+    //return property_get_int32("audio.pcm.card", PCM_CARD);
+    return 0;
 }
 
 static int get_pcm_device() {
-    return property_get_int32("audio.pcm.device", PCM_DEVICE);
+    //return property_get_int32("audio.pcm.device", PCM_DEVICE);
+    return 0;
 }
 
 static int check_output_config(struct audio_config *audio_config) {
