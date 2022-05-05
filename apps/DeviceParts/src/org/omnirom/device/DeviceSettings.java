f18932b61ccecad16dc97c05da6e3683f346e39d
maxwen <max.weninger@gmail.com>
2022-05-05 22:20:41 +0200

    DeviceParts: fix the fix for orientation settings
    we must release the user rotation lock
    
    Change-Id: I0bc13558ca102f19350192ce5030af610808f3c8

--- apps/DeviceParts/src/org/omnirom/device/DeviceSettings.java
+++ apps/DeviceParts/src/org/omnirom/device/DeviceSettings.java
@@ -34,6 +34,7 @@ import android.provider.Settings;
 import android.view.Display;
 import android.view.Display.Mode;
 import android.view.View;
+import android.view.IWindowManager;
 import android.view.Surface;
 import android.util.Log;
 
@@ -60,6 +61,7 @@ public class DeviceSettings extends PreferenceFragment implements
     private static final String CPU_SYSFS_FREQUENCIES = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
     private static final String CPU_SYSFS_MAX_FREQ = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
 
+    private IWindowManager mWindowManager;
     private ListPreference mRotationLock;
     private ListPreference mBootMode;
     //private ListPreference mAudioCard;
@@ -71,7 +73,8 @@ public class DeviceSettings extends PreferenceFragment implements
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.main, rootKey);
-
+        mWindowManager = IWindowManager.Stub.asInterface(
+                ServiceManager.getService(Context.WINDOW_SERVICE));
         mRotationLock = (ListPreference) findPreference(KEY_ROTATION_LOCK);
         mRotationLock.setOnPreferenceChangeListener(this);
         mRotationLock.setSummary(mRotationLock.getEntry());
@@ -121,28 +124,34 @@ public class DeviceSettings extends PreferenceFragment implements
         if (preference == mRotationLock) {
             String value = (String) newValue;
             int rotationLockValue = Integer.valueOf(value);
-            int accelerometerRotation = 0;
             int userRotation = 0;
-            if (rotationLockValue == 0) {
-                accelerometerRotation = 0;
-                userRotation = Surface.ROTATION_0;
-            } else if (rotationLockValue == 1) {
-                accelerometerRotation = 1;
-                userRotation = mNativeOrientation == 0 ? Surface.ROTATION_0 : Surface.ROTATION_270;
-            } else if (rotationLockValue == 2) {
-                accelerometerRotation = 1;
-                userRotation = mNativeOrientation == 0 ? Surface.ROTATION_270 : Surface.ROTATION_0;
-            } else if (rotationLockValue == 3) {
-                accelerometerRotation = 1;
-                userRotation = mNativeOrientation == 0 ? Surface.ROTATION_90 : Surface.ROTATION_180;
-            } else if (rotationLockValue == 4) {
-                accelerometerRotation = 1;
-                userRotation = mNativeOrientation == 0 ? Surface.ROTATION_180 : Surface.ROTATION_90;
+
+            try {
+                if (rotationLockValue == 0) {
+                    mWindowManager.freezeRotation(Surface.ROTATION_0);
+                    mWindowManager.setFixedToUserRotation(Display.DEFAULT_DISPLAY, IWindowManager.FIXED_TO_USER_ROTATION_DEFAULT);
+                    mWindowManager.thawRotation();
+                } else if (rotationLockValue == 1) {
+                    userRotation = mNativeOrientation == 0 ? Surface.ROTATION_0 : Surface.ROTATION_90;
+                    mWindowManager.freezeRotation(userRotation);
+                    mWindowManager.setFixedToUserRotation(Display.DEFAULT_DISPLAY, IWindowManager.FIXED_TO_USER_ROTATION_ENABLED);
+                } else if (rotationLockValue == 2) {
+                    userRotation = mNativeOrientation == 0 ? Surface.ROTATION_270 : Surface.ROTATION_0;
+                    mWindowManager.freezeRotation(userRotation);
+                    mWindowManager.setFixedToUserRotation(Display.DEFAULT_DISPLAY, IWindowManager.FIXED_TO_USER_ROTATION_ENABLED);
+                } else if (rotationLockValue == 3) {
+                    userRotation = mNativeOrientation == 0 ? Surface.ROTATION_90 : Surface.ROTATION_180;
+                    mWindowManager.freezeRotation(userRotation);
+                    mWindowManager.setFixedToUserRotation(Display.DEFAULT_DISPLAY, IWindowManager.FIXED_TO_USER_ROTATION_ENABLED);
+                } else if (rotationLockValue == 4) {
+                    userRotation = mNativeOrientation == 0 ? Surface.ROTATION_180 : Surface.ROTATION_270;
+                    mWindowManager.freezeRotation(userRotation);
+                    mWindowManager.setFixedToUserRotation(Display.DEFAULT_DISPLAY, IWindowManager.FIXED_TO_USER_ROTATION_ENABLED);
+                }
+                mRotationLock.setSummary(mRotationLock.getEntries()[rotationLockValue]);
+            } catch (RemoteException e){
+                Log.e(TAG, "freezeRotation", e);
             }
-            final ContentResolver res = getActivity().getContentResolver();
-            Settings.System.putInt(res, Settings.System.ACCELEROMETER_ROTATION, accelerometerRotation);
-            Settings.System.putInt(res, Settings.System.USER_ROTATION, userRotation);
-            mRotationLock.setSummary(mRotationLock.getEntries()[rotationLockValue]);
         /*} else if (preference == mAudioCard) {
             String value = (String) newValue;
             SystemProperties.set(AUDIO_CARD_OVERRIDE_PROPERTY, value);
