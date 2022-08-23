2d6d6c83874c8b43475c0b15dcf2c3a8f8d0a7e1
maxwen <max.weninger@gmail.com>
2022-08-23 17:40:54 +0200

    rpi4: Move Widevine DRM HAL to AIDL
    Change-Id: I054bc1fb8331065b30ecbc2fa24dc22d020214c9

--- device.mk
+++ device.mk
@@ -90,9 +90,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.widevine \
+    android.hardware.drm-service.clearkey
 
 # Enable USB Camera
 PRODUCT_PACKAGES += \
