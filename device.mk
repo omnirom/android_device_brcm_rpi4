be2ec7b0158d02c5963091f740acb06c94f5ee8a
maxwen <max.weninger@gmail.com>
2022-08-24 21:26:26 +0200

    bluetooth: Add BT config using sysprops
    Change-Id: I0b064c87bd8aed0f32e2a2dc5bb5ad786010d7ee

--- device.mk
+++ device.mk
@@ -38,6 +38,26 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/bluetooth/firmware/BCM4345C0.hcd:vendor/etc/firmware/brcm/BCM4345C0.hcd \
     $(DEVICE_PATH)/bluetooth/firmware/BCM4345C5.hcd:vendor/etc/firmware/brcm/BCM4345C5.hcd
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.core.gap.le.privacy.enabled=false \
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.bap.broadcast.assist.enabled=true \
+    bluetooth.profile.bap.unicast.client.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.ccp.server.enabled=true \
+    bluetooth.profile.csip.set_coordinator.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hap.client.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.mcp.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.vcp.controller.enabled=true
+
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
